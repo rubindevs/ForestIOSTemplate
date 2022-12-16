64d5646cd16def0d4f736cc9ef2153e980e60ce7
rubindevs <rubindevs@gmail.com>
2022-12-16 12:43:14 +0900

    resolve error

---
+++ Sources/ForestIOSTemplate/UI/Buttons/VVHView.swift
@@ -0,0 +1,17 @@
+//
+//  ILHButton.swift
+//  TemplateLab
+//
+//  Created by 염태규 on 2022/11/27.
+//
+
+// Image Label Horozontal
+import Foundation
+import UIKit
+
+public class VVHView: BaseView {
+    
+    public override func initViews(parent: UIViewController?) {
+        
+    }
+}
