ace9d88f8f6cccd238a6ac8d03a8a952652b7f4b
rubindevs <rubindevs@gmail.com>
2022-12-18 19:20:02 +0900

    add public

---
+++ Sources/ForestIOSTemplate/SearchTextField.swift
@@ -0,0 +1,54 @@
+//
+//  File.swift
+//  
+//
+//  Created by 염태규 on 2022/12/18.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+public class SearchTextField: BaseView, UITextFieldDelegate {
+    
+    public var view_image = UIImageView()
+    public var textField = UITextField()
+    public var callback: ((String?) -> Void)? = nil
+    
+    public override func initViews(parent: UIViewController?) {
+        addSubview(view_image)
+        view_image.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
+        
+        addSubview(textField)
+        textField.snp.makeConstraints { make in
+            make.leading.equalTo(view_image.snp.trailing)
+            make.top.bottom.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        textField.delegate = self
+        textField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+
+    }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        callback?(textField.text)
+    }
+    
+    public func set(image: ViewImage?, interval: CGFloat, callback: @escaping (String?) -> Void) {
+        self.view_image.isHidden = image == nil
+        self.callback = callback
+        self.view_image.snp.remakeConstraints { make in
+            image?.inflateConstraints(make)
+        }
+        
+        self.textField.snp.remakeConstraints { make in
+            make.leading.equalTo(view_image.snp.trailing).offset(interval)
+            make.top.bottom.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+    }
+}
+
