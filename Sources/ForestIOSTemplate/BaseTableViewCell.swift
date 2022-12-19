1d3a873cf4fa0aadc410a4b5846c6f015ae51c76
rubindevs <rubindevs@gmail.com>
2022-12-19 15:26:12 +0900

    base tableview cell

---
+++ Sources/ForestIOSTemplate/BaseTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  
+//
+//  Created by 염태규 on 2022/12/19.
+//
+
+import Foundation
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initViews()
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initViews() {
+    }
+}
