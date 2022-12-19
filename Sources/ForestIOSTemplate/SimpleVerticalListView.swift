930b0147acf6e3fef7d6c6ffe01aea7a9c4d2c82
rubindevs <rubindevs@gmail.com>
2022-12-19 16:16:51 +0900

    simpleverticallist open

--- Sources/ForestIOSTemplate/SimpleVerticalListView.swift
+++ Sources/ForestIOSTemplate/SimpleVerticalListView.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 import SnapKit
 
-class SimpleVerticalListView: BaseView, UITableViewDataSource, UITableViewDelegate {
+open class SimpleVerticalListView: BaseView, UITableViewDataSource, UITableViewDelegate {
     
     let table_main = UITableView(frame: .zero, style: .grouped)
     var count: (() -> Int)? = nil
@@ -17,7 +17,7 @@ class SimpleVerticalListView: BaseView, UITableViewDataSource, UITableViewDelega
     var cellData: ((UITableView, IndexPath) -> UITableViewCell)? = nil
     var onTouch: ((IndexPath) -> Void)? = nil
     
-    override func initViews(parent: UIViewController?) {
+    open override func initViews(parent: UIViewController?) {
         addSubview(table_main)
         table_main.snp.makeConstraints { make in
             make.edges.equalToSuperview()
@@ -35,31 +35,31 @@ class SimpleVerticalListView: BaseView, UITableViewDataSource, UITableViewDelega
         self.onTouch = onTouch
     }
     
-    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         return nil
     }
 
-    func numberOfSections(in tableView: UITableView) -> Int {
+    public func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 0
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return count?() ?? 0
     }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return cellHeight?(indexPath) ?? 0
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         return cellData?(tableView, indexPath) ?? UITableViewCell()
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         onTouch?(indexPath)
     }
 }
