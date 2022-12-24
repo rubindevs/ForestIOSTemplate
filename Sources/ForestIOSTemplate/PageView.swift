1764d081dfaa6f8724cf0a6d63c1f63ef594a941
rubindevs <rubindevs@gmail.com>
2022-12-24 13:39:45 +0900

    pageview

---
+++ Sources/ForestIOSTemplate/PageView.swift
@@ -0,0 +1,130 @@
+//
+//  File.swift
+//  
+//
+//  Created by 염태규 on 2022/12/24.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+open class PageView<T, U: BaseLayout>: BaseView {
+    
+    let controller_page = PagingViewController<T, U>()
+    let layout_page = UIView()
+    var parent: UIViewController?
+    
+    open override func initViews(parent: UIViewController? = nil) {
+        self.parent = parent
+        addSubview(layout_page)
+        layout_page.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    public func set(parent: UIViewController, items: [T], inflate: @escaping (T, U) -> Void) {
+        if !parent.has(controller_page) {
+            parent.add(controller_page, view: layout_page, topView: nil)
+        }
+        controller_page.set(items: items, inflate: inflate)
+    }
+    
+    
+    class PagingViewController<T, U: BaseLayout>: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+
+        var pages: [UIViewController] = []
+        var items: [T] = []
+
+        override init(transitionStyle style: UIPageViewController.TransitionStyle, navigationOrientation: UIPageViewController.NavigationOrientation, options: [UIPageViewController.OptionsKey : Any]? = nil) {
+            super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        }
+
+        required init?(coder: NSCoder) {
+            super.init(coder: coder)
+        }
+
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            dataSource = self
+            delegate = self
+        }
+        
+        func set(items: [T], inflate: @escaping (T, U) -> Void) {
+            if !items.isEmpty {
+                pages = (0..<items.count).map { PageViewController(index: $0, item: items[$0], inflate: inflate) }
+                setViewControllers([pages[0]], direction: .forward, animated: false, completion: nil)
+            }
+        }
+
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+            let previousIndex = viewControllerIndex - 1
+            guard previousIndex >= 0 else { return pages.last }
+            guard pages.count > previousIndex else { return nil }
+            return pages[previousIndex]
+        }
+
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+            let nextIndex = viewControllerIndex + 1
+            guard nextIndex < pages.count else { return pages.first }
+            guard pages.count > nextIndex else { return nil }
+            return pages[nextIndex]
+        }
+        
+        func presentationCount(for pageViewController: UIPageViewController) -> Int {
+            return pages.count
+        }
+        
+        func next() {
+            guard let currentViewController = self.viewControllers?.first else { return }
+            guard let nextViewController = dataSource?.pageViewController( self, viewControllerAfter: currentViewController ) else { return }
+
+            // Has to be set like this, since else the delgates for the buttons won't work
+            setViewControllers([nextViewController], direction: .forward, animated: true, completion: { completed in self.delegate?.pageViewController?(self, didFinishAnimating: true, previousViewControllers: [], transitionCompleted: completed) })
+        }
+        
+        func prev() {
+            guard let currentViewController = self.viewControllers?.first else { return }
+            guard let previousViewController = dataSource?.pageViewController( self, viewControllerBefore: currentViewController ) else { return }
+
+            // Has to be set like this, since else the delgates for the buttons won't work
+            setViewControllers([previousViewController], direction: .reverse, animated: true, completion:{ completed in self.delegate?.pageViewController?(self, didFinishAnimating: true, previousViewControllers: [], transitionCompleted: completed) })
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+            if completed {
+                if let currentViewController = pageViewController.viewControllers?.first,
+                   let index = pages.firstIndex(of: currentViewController) {
+//                    if let parent = parent as? HomeViewController {
+//                        parent.layout.view_banners.set(index: index, total: pages.count)
+//                    }
+                }
+            }
+        }
+    }
+
+    class PageViewController<T, U: BaseLayout>: UIViewController {
+        
+        var index = 0
+        var item: T? = nil
+        var layout = U()
+        var inflate: ((T, U) -> Void)? = nil
+        
+        convenience init(index: Int, item: T, inflate: @escaping (T, U) -> Void) {
+            self.init()
+            self.index = index
+            self.item = item
+            self.inflate = inflate
+        }
+        
+        override func viewDidLoad() {
+            layout.initViews(view)
+            if let item = item {
+                inflate?(item, layout)
+            }
+        }
+    }
+}
