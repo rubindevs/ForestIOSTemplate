d6aba8019b6cfd807b95423c3ea3b934932c37d4
rubindevs <rubindevs@gmail.com>
2022-12-27 12:45:18 +0900

    lview

---
+++ Sources/ForestIOSTemplate/LView.swift
@@ -0,0 +1,30 @@
+//
+//  File.swift
+//  
+//
+//  Created by 염태규 on 2022/12/27.
+//
+
+import Foundation
+import SnapKit
+import UIKit
+
+public class LView: BaseView {
+    
+    public var label = UILabel()
+    
+    public override func initViews(parent: UIViewController?) {
+        addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+    }
+    
+    public func set(text: ViewText, intervalH: CGFloat, intervalV: CGFloat) {
+        label.snp.remakeConstraints { make in
+            text.alignV.inflateConstraints(make, voffset: intervalV)
+            text.alignH.inflateConstraints(make, hoffset: intervalH)
+        }
+        text.setToLabel(label)
+    }
+}
