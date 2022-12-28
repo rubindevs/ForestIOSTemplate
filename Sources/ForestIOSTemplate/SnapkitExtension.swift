7c382bf84044fe50e3482d314c44975adf6662fb
rubindevs <rubindevs@gmail.com>
2022-12-28 13:22:08 +0900

    constaints items add

--- Sources/ForestIOSTemplate/SnapkitExtension.swift
+++ Sources/ForestIOSTemplate/SnapkitExtension.swift
@@ -10,6 +10,10 @@ import SnapKit
 public extension ConstraintViewDSL {
     
     func makeEasyConstraints(topView: UIView? = nil, bottomView: UIView? = nil, leadingView: UIView? = nil, trailingView: UIView? = nil, completion: (_ width: Int, _ height: Int) -> Void, _ formats: [String]) {
+        self.makeEasyConstraintItems(top: topView?.snp.bottom, bottom: bottomView?.snp.top, leading: leadingView?.snp.trailing, trailing: trailingView?.snp.leading, completion: completion, formats)
+    }
+    
+    func makeEasyConstraintItems(top: ConstraintItem? = nil, bottom: ConstraintItem? = nil, leading: ConstraintItem? = nil, trailing: ConstraintItem? = nil, completion: (_ width: Int, _ height: Int) -> Void, _ formats: [String]) {
         self.makeConstraints { make in
             var fixedWidth = 0
             var fixedHeight = 0
@@ -19,26 +23,26 @@ public extension ConstraintViewDSL {
                 let offset = Int(format[format.index(format.startIndex, offsetBy: inherition ? 2 : 1)...]) ?? 0
                 switch operation {
                 case "l":
-                    if leadingView != nil {
-                        make.leading.equalTo(leadingView!.snp.trailing).offset(offset)
+                    if leading != nil {
+                        make.leading.equalTo(leading!).offset(offset)
                     } else {
                         make.leading.equalBy(inherition, offset)
                     }
                 case "r":
-                    if trailingView != nil {
-                        make.trailing.equalTo(trailingView!.snp.leading).offset(offset)
+                    if trailing != nil {
+                        make.trailing.equalTo(trailing!).offset(offset)
                     } else {
                         make.trailing.equalBy(inherition, offset)
                     }
                 case "t":
-                    if topView != nil {
-                        make.top.equalTo(topView!.snp.bottom).offset(offset)
+                    if top != nil {
+                        make.top.equalTo(top!).offset(offset)
                     } else {
                         make.top.equalBy(inherition, offset)
                     }
                 case "b":
-                    if bottomView != nil {
-                        make.bottom.equalTo(bottomView!.snp.top).offset(offset)
+                    if bottom != nil {
+                        make.bottom.equalTo(bottom!).offset(offset)
                     } else {
                         make.bottom.equalBy(inherition, offset)
                     }
