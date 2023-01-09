2740fc37d84a0d9902eb2334649709e64127105d
rubindevs <rubindevs@gmail.com>
2023-01-09 16:32:24 +0900

    simple title section view

---
+++ Sources/ForestIOSTemplate/SimpleTitleSectionView.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  
+//
+//  Created by 염태규 on 2023/01/09.
+//
+
+import Foundation
+import UIKit
+import SnapKit
+
+public class SimpleTitleSectionView: BaseTableViewHeaderFooterView {
+    
+    var label_title = UILabel()
+    
+    public override func initViews() {
+        addSubview(label_title)
+        label_title.snp.makeEasyConstraints("ls0", "bs0")
+    }
+    
+    func set(title: ViewText, intervalH: CGFloat, intervalV: CGFloat) {
+        label_title.snp.remakeConstraints { make in
+            title.alignH.inflateConstraints(make, hoffset: intervalH)
+            title.alignV.inflateConstraints(make, voffset: intervalV)
+        }
+        title.setToLabel(label_title)
+    }
+}
