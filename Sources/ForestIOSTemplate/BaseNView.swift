7acad01d4b189bdb45fab1987b37da9610735f97
rubindevs <rubindevs@gmail.com>
2023-02-01 11:17:58 +0900

    basenview change

--- Sources/ForestIOSTemplate/BaseNView.swift
+++ Sources/ForestIOSTemplate/BaseNView.swift
@@ -13,8 +13,10 @@ open class BaseNView<T: NCodable>: BaseView {
     public let id = UUID().uuidString
     
     var local_unregister: (String) -> Void = { uuid in }
-    open func register() {
-        local_unregister = DataProvider.shared.register(view: self)
+    open func register() { // TODO: async
+        Task {
+            local_unregister = await DataProvider.shared.register(view: self)
+        }
     }
     
     open func unregister() {
@@ -22,21 +24,11 @@ open class BaseNView<T: NCodable>: BaseView {
     }
     
     open func inflate(data: T) {
-        self.onInflate?(data)
+        // override function : no code
     }
     
     open func inflate(datas: [T]) {
-        self.onInflateList?(datas)
-    }
-    
-    var onInflate: ((T) -> Void)?
-    open func setOnInflate(callback: @escaping (T) -> Void) {
-        self.onInflate = callback
-    }
-    
-    var onInflateList: (([T]) -> Void)?
-    open func setOnInflateList(callback: @escaping ([T]) -> Void) {
-        self.onInflateList = callback
+        // override function : no code
     }
     
     open func onShowLoading() async {
